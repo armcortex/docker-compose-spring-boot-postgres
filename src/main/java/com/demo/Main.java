ae9ab27f81c9f077f1221cc6e7a591ab547887fb
armcortex <armcortexfpga@gmail.com>
2023-11-02 23:36:22 +0800

    rename Main.java

--- src/main/java/com/demo/Main.java
+++ src/main/java/com/demo/Main.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class main {
+public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(main.class);
+        SpringApplication.run(Main.class);
     }
 }
