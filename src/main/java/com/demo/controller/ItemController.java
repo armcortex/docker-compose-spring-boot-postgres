07a0ebdf142cee57c2749d5789a56d42c8425e48
armcortex <armcortexfpga@gmail.com>
2023-11-03 20:39:47 +0800

    add API's description code

--- src/main/java/com/demo/controller/ItemController.java
+++ src/main/java/com/demo/controller/ItemController.java
@@ -8,6 +8,12 @@ import com.demo.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.Optional;
 
 @RestController
@@ -17,13 +23,23 @@ public class ItemController {
     @Autowired
     private ItemService itemService;
 
+    @Operation(summary="Add a new item", description="Creates a new item in the database")
+    @ApiResponse(responseCode="200", description="Item created",
+                    content={@Content(mediaType="application/json",
+                    schema=@Schema(implementation=Item.class))})
     @PostMapping
     public Item addItem(@RequestBody Item item) {
         return itemService.saveItem(item);
     }
 
+    @Operation(summary="Get an item by ID", description="Returns a single item")
+    @ApiResponse(responseCode="200", description="Item found",
+                    content={@Content(mediaType="application/json",
+                    schema=@Schema(implementation=Item.class))})
+    @ApiResponse(responseCode="404", description="Item not found")
     @GetMapping
-    public Optional<Item> getItemById(@RequestParam(name = "id") long id) {
+    public Optional<Item> getItemById(@Parameter(description="ID of the item to be obtained", required=true)
+                                      @RequestParam(name="id") long id) {
         return itemService.getItemById(id);
     }
 }
