8756c1907a324fad382b3cbcc2439a70687e0eb9
armcortex <armcortexfpga@gmail.com>
2023-11-04 18:22:19 +0800

    locust server random stress POST test ok

--- script/locust_items_test.py
+++ script/locust_items_test.py
@@ -1,9 +1,14 @@
+import random
 from locust import HttpUser, task, between
+
+DATA_COUNT = 10000
 
 
 class WebsiteUser(HttpUser):
     wait_time = between(1, 5)
 
     @task
-    def load_item(self):
-        self.client.get("/item?id=1")
+    def get_item(self):
+        item_id = random.randint(1, DATA_COUNT)
+        self.client.get(f'/item?id={item_id}')
+
